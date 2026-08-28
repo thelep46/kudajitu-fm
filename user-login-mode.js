ded5b25c8dbf0e32e3e9c8e7aab12dc9f8d1c8ef
Thelep <vmpaaudara@gmail.com>
2026-08-28 22:43:11 +0700

    Fix user login mode to use Cloudflare API proxy

--- user-login-mode.js
+++ user-login-mode.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const GAS='https://script.google.com/macros/s/AKfycbyUB8drjL1dSJedYjKIKjVc5gzIE3Pe-QS0FF8o1_zU4NkAweGLFquhHLfy1Nt_eITA-Q/exec';
+const GAS='/api/gas';
 const TOKEN_KEY='kudajitu_member_token_v1';
 let loginMode='open';
 function token(){try{return localStorage.getItem(TOKEN_KEY)||''}catch(e){return''}}
