6ad0e6b5018a4cd4581371b1927e0516dc935926
Thelep <vmpaaudara@gmail.com>
2026-09-04 19:26:46 +0700

    Fix clean /youtube-mapping route with dedicated Pages Function

---
+++ functions/youtube-mapping.js
@@ -0,0 +1,4 @@
+export async function onRequest(context) {
+  const url = new URL('/youtube-mapping.html', context.request.url);
+  return context.env.ASSETS.fetch(new Request(url, context.request));
+}
