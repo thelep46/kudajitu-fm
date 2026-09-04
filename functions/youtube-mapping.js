a56fe74f447ff4b8e7a9dcf5eb15a74bbff45898
Thelep <vmpaaudara@gmail.com>
2026-09-04 19:32:19 +0700

    Remove recursive mapping route handler

--- functions/youtube-mapping.js
+++
@@ -1,4 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL('/youtube-mapping.html', context.request.url);
-  return context.env.ASSETS.fetch(new Request(url, context.request));
-}
